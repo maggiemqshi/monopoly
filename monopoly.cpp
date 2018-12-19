15b5cdc797391b0e9495a534a7f5fd783869ba0a
eeyeoh <eyeoh@usc.edu>
2018-12-19 11:36:42 -0800

    Update monopoly.cpp

--- monopoly.cpp
+++ monopoly.cpp
@@ -4,11 +4,31 @@ using namespace std;
 
 int main()
 {
-	int num;
-	cin >> num;
-	
-	for(int i = 0; i < num; i++)
-	{
-		cout << "Hello World!" << endl;
-	}
+    srand(time(NULL));
+    
+    int num;
+    cin >> num;
+    
+    int steps;
+    cin >> steps;
+    
+    vector <Player> players;
+    /*cout << "Ee is the cutest!\n";
+    cout << "Maggie is the cutest! Ee is not cute at all." << endl;
+    cout << "HOW DARE" << endl;*/
+    
+    for (int i = 0; i < num; i++)
+    {
+        players.push_back(Player(i+1));      
+    }
+    
+    
+    for (int i = 0; i < steps; i++)
+    {   
+        for (int j = 0; j < players.size(); j++)
+        {
+            players.at(j).move();
+        }
+        
+    }
 }
